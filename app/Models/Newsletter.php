3f2bf5658726ee1a999b1558f5f4cad4e88c3bab
Lewis Munyi <lewismunyiii@gmail.com>
2024-03-31 02:49:15 +0300

    Add tags to newsletters

--- app/Models/Newsletter.php
+++ app/Models/Newsletter.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Newsletter extends Model
@@ -16,4 +17,9 @@ class Newsletter extends Model
         'content',
         'status',
     ];
+
+    public function tags(): MorphMany
+    {
+        return $this->morphMany(Tag::class, 'taggable');
+    }
 }
--- app/Models/Tag.php
+++ app/Models/Tag.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class Tag extends Model
 {
@@ -15,6 +16,11 @@ class Tag extends Model
     public function recipes(): BelongsToMany
     {
         return $this->belongsToMany(Recipe::class, 'recipe_tag', 'tag_id', 'recipe_id')->withTimestamps();
+    }
+
+    public function newsletters(): MorphMany
+    {
+        return $this->morphMany(Newsletter::class, 'taggable');
     }
 
     public function taggable()
