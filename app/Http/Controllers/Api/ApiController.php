9fd3f0cb15f17a9b2ae33a436e646f1720baec09
Lewis Munyi <lewismunyiii@gmail.com>
2024-03-30 21:51:35 +0300

    Fix CommentController

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers\Api;
 
-abstract class ApiController
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class ApiController extends BaseController
 {
-    //
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
