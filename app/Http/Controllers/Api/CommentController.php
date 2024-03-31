fd875dba6c64ce8962301b3ca5b69b64ff9a5004
Lewis Munyi <lewismunyiii@gmail.com>
2024-03-31 12:09:06 +0300

    Bug fix

--- app/Http/Controllers/Api/CommentController.php
+++ app/Http/Controllers/Api/CommentController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Models\Comment;
 use Illuminate\Http\Request;
 
-class ApiCommentController extends ApiController
+class CommentController extends ApiController
 {
     /**
      * Display a listing of the resource.
